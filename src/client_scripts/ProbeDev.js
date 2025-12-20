c494b5fb3e036dfd24e6865b4eccad5e8d0ab119
SikongJueluo <selfconfusion@gmail.com>
2025-12-20 16:28:57 +0800

    chore(project): fully support ProbeJS v7.0 (Also ProbeJS Legacy v5.2.3)

---
+++ src/client_scripts/ProbeDev.js
@@ -0,0 +1,15 @@
+/**
+ * WARN: Need Probejs v7.0
+ *
+ * NOTE: This file is used for development. There is no need to load.
+ * Events execute when ProbeJS dumps.
+ *
+ * Author: SikongJueluo
+ * License: MIT
+ */
+
+ProbeEvents.assignType((event) => {});
+
+ProbeEvents.modifyClass((event) => {});
+
+ProbeEvents.snippets((event) => {});
